0db318ab2183a615c5d120d47aeed801e02c741a
NumdyPixe1 <numdao.ratcha@gmail.com>
2026-03-21 17:44:59 +0700

    add: GET, GET{id}

---
+++ app/api/artifacts/route.ts
@@ -0,0 +1,55 @@
+import supabase from "@/lib/supabase-client";
+import { NextResponse } from "next/server";
+
+// http://localhost:3000/api/artifacts
+export const GET = async (req: Request) => {
+    try {
+        const { data, error } = await supabase.from('Artifacts').select('*').order("created_at", { ascending: false });
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
+        }
+        return NextResponse.json(data);
+    }
+    catch (e: any) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export const POST = async (req: Request) => {
+    try {
+        // 1. รับข้อมูล JSON จาก Body ของ Postman
+        const body = await req.json();
+        const { title, art_style, material, location_found, location, description, image_url } = body;
+        // 2. ตรวจสอบข้อมูลเบื้องต้น
+        if (!title) {
+            return NextResponse.json(
+                { error: "กรุณาระบุชื่อวัตถุ" },
+                { status: 400 }
+            );
+        }
+        // 3. บันทึกลง Supabase
+        const { data, error } = await supabase
+            .from('Artifacts')
+            .insert({ title, art_style, material, location_found, location, description, image_url })
+        return NextResponse.json({ message: "Add artifact successfully" }, { status: 201 });
+    }
+    catch (e: any) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export const PUT = async (req: Request) => {
+    try {
+
+    }
+    catch (e: any) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export const DELETE = async (req: Request) => {
+    try { }
+    catch (e: any) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
